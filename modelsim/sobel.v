987260d1d00342a13e4f8523c3e1a663421291e3
Emma Smith <emmarsmi@usc.edu>
2016-04-03 16:21:07 -0700

    Sobel fleshed out
    Sobel inner and outer parts are compiling and should work. I have not
    written the test bench.

--- modelsim/sobel.v
+++ modelsim/sobel.v
@@ -1,104 +1,71 @@
 module sobel #(parameter WIDTH, parameter DEPTH)(
-
-	input [WIDTH*DEPTH:0] inputImage,
-	output bmpOutput
+	input clk,
+	input [7:0] threshold,
+	input [7:0] pixel, 
+	output [WIDTH*DEPTH:0] bmpOutput 
 
 );
 
-integer Y;
+reg [WIDTH*DEPTH:0] out;
+reg [7:0] SUM;
 integer X;
-integer I;
-integer J;
-integer sumX;
-integer sumY;
-integer SUM;
+integer Y;
+reg enabled = 1'b1;
 
-integer theAbs;
+reg[7:0] inputImage [WIDTH*DEPTH:0];
 
-integer xSum;
-integer ySum;
+assign bmpOutput = out;
 
-task absX;
-	input integer a;
+integer i = 0;
+
+always@(posedge clk)
+begin
+	if(enabled)
+	begin		
+		inputImage[i] = pixel;
+		i = i+1;
+		if(i >= WIDTH*HEIGHT)
+			enabled = 1'b0;
+	end
+end
+
+reg isEdge;
+task applyThresh;
+	input integer t;
+	input [7:0] val;
 	begin
-		if( a < 0 )
-			xSum = a * -1;
+		if( val > t )
+			isEdge = 1;
 		else
-			xSum = a;
+			isEdge = 0;
 	end
 endtask
-
-
-task absY;
-	input integer a;
-	begin
-		if( a < 0 )
-			ySum = a * -1;
-		else
-			ySum = a;
-	end
-endtask
-
-
-
 
 initial
 begin
-
-for(Y=0; Y<=(DEPTH-1); Y = Y+1)
-begin
-	for(X=0; X<=(WIDTH-1); X= X+1)
+	for(Y=0; Y<=(DEPTH-1); Y = Y+1)
 	begin
-		sumX = 0;
-		sumY = 0;
-
-        // image boundaries
-		if(Y==0 || Y==(DEPTH-1))
-			SUM = 0;
-		else if(X==0 || X==(WIDTH-1))
-			SUM = 0;
-
-		// Convolution starts here
-		else
+		for(X=0; X<=(WIDTH-1); X= X+1)
 		begin
-
-			//-------X GRADIENT APPROXIMATION-----
-			for(I=-1; I <= 1; I = I + 1)
-				begin
-					for(J=-1; J<=1; J = J + 1)
-					begin
-						//sumX = sumX + (integer)( (*(originalImage.data + X + I + (Y + J)*WIDTH)) * GX[I+1][J+1]);
-						sumX = sumX + 2; // Random garbage
-					end
-			end
-
-			//-------Y GRADIENT APPROXIMATION------
-			for(I=-1; I<=1; I = I + 1)
+	
+	        // image boundaries
+			if(Y==0 || Y==(DEPTH-1))
+				SUM = 0;
+			else if(X==0 || X==(WIDTH-1))
+				SUM = 0;	
+			else
 			begin
-				for(J=-1; J<=1; J = J + 1)
-				begin
-					//sumY = sumY + (int)( (*(originalImage.data + X + I + (Y + J)*originalImage.cols)) * GY[I+1][J+1]);
-				end
+				innerSobel(inputImage[Y*WIDTH-WIDTH+X-1],inputImage[Y*WIDTH-WIDTH+X],inputImage[Y*WIDTH-WIDTH+X+1],inputImage[Y*WIDTH+X-1],inputImage[Y*WIDTH+X],inputImage[Y*WIDTH+X+1],inputImage[Y*WIDTH+WIDTH+X-1],inputImage[Y*WIDTH+WIDTH+X],inputImage[Y*WIDTH+WIDTH+X+1], SUM);
+				
 			end
-
-			//---GRADIENT MAGNITUDE APPROXIMATION (Myler p.218)---
-			absX(sumX);
-			absY(sumY);
-
-			//SUM = abs(sumX) + abs(sumY);
-			SUM = xSum + ySum;
+			applyThresh(threshold, SUM);
+			out[Y*WIDTH+X] <= isEdge;
 		end
 
-		if(SUM>255) SUM=255;
-		if(SUM<0) SUM=0;
-
-		//*(edgeImage.data + X + Y*originalImage.cols) = 255 - (unsigned char)(SUM);
-		//fwrite((edgeImage.data + X + Y*originalImage.cols),sizeof(char),1,bmpOutput);
-
 	end
-
 end
 
-end
+
+
 
 endmodule
---
+++ modelsim/tb_testing.v
@@ -0,0 +1,23 @@
+module tb_testing;
+
+parameter clk_cycle=10;
+reg clk;
+reg [7:0] thresh;
+reg [7:0] pixelIn;
+reg [] theOutput;
+
+mySobel #(,) sobel(
+	.clk(clock),
+	.threshold(thresh),
+	.pixel(pixelIn), 
+	.bmpOutput(theOutput) 
+
+);
+
+
+
+
+
+
+
+endmodule
